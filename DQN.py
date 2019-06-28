9d9d5a7f730e628f96067ee0a04427026a4b8fb4
mohamednagi <eng.m.nagi@gmail.com>
2019-06-29 01:11:19 +0200

    cartpole

--- DQN.py
+++ DQN.py
@@ -1,9 +1,14 @@
 import random
+from cgi import maxlen
+
 import gym
 import numpy as np
 from collections import deque
 import tensorflow as tf
-from pexpect import searcher_re
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+import os
 
 """
 breakout -actions_space = 4 val
@@ -14,34 +19,68 @@ breakout -observation_ = 1D-Array of size = 128
 """
 
 class Deep_Q_Network(object):
-    """
-    This class impelements the Model of the Agent for our Deep Q network
-    Session =
-    """
-    def __init__(self, session, num_actions, learning_rate = 1e-3, history_size = 4, screen_height = 84, screen_width = 84, gamma = 0.98):
-        self.session = session
-        self.num_actions = num_actions
-        self.learning_rate = learning_rate
-        self.history_size = history_size
-        self.screen_height = screen_height
-        self.screen_width = screen_width
-        self.gamma = gamma
 
-        self.build_prediction_network()
-        self.build_target_network()
-        self.build_training()
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
 
-    def build_prediction_network(self):
-        with tf.variable_scope("prediction_network"):
-            self.states = tf.placeholder(tf.float32, shape=())
+        #experience Replay_table
+        self.experience_table = deque(maxlen=5000)
 
+        # discount future reward
+        self.gamma = 0.9
 
+        # exploration
+        self.epsilon = 1.0
+        # minimizing exploration
+        self.minimize_epsilon = 0.995
+        # minimum exploration
+        self.minimum_epsilon = 0.01
 
-    def build_target_network(self):
-        pass
+        self.learning = 0.001
 
-    def build_training(self):
-        pass
+        self.model = self.build_model()
 
+    def build_model(self):
+        model = Sequential()
 
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
 
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning))
+
+        return model
+
+    def store_experience(self, state, action, reward, next_state, done):
+        self.experience_table.append((state, action, reward, next_state, done))
+
+    def take_action(self, state):
+        #takes a random number and checks if its less than epsilon takes a random explatory action
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        #else take action predicted by our model
+        action_values = self.model.predict(state)
+        # return predicted action that gives the maximum reward
+        return np.argmax(action_values[0])
+
+    def replay(self, batch_size):
+        mini_batch = random.sample(self.experience_table, batch_size)
+        for state, action, reward, next_state, done in mini_batch:
+            target = reward
+            if not done:
+                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+                # pass the batch of the preprocessed state to policy network
+                future_target = self.model.predict(state)
+                future_target[0][action] = target
+
+                self.model.fit(state, future_target, epochs=1, verbose=0)
+
+            if self.epsilon > self.minimum_epsilon:
+                self.epsilon *= self.minimize_epsilon
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
--- testing2.py
+++ testing2.py
@@ -1,10 +1,55 @@
+import gym
+import os
+from DQN import Deep_Q_Network
 import numpy as np
 
-x = np.array([[[1],[2],[3]],[[4],[5],[6]]])
-print (x)
-print(x.shape)
-x_1 =x[1:]
-x_2 = x[:2]
-print("xxxxxxxxxx")
+env = gym.make('CartPole-v0')
 
-print(x[:0])
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n
+print(state_size)
+
+batch_size = 32
+
+n_episode = 100
+
+OP_dir = 'cartpole_output/'
+
+if not os.path.exists(OP_dir):
+    os.mkdir(OP_dir)
+
+agent = Deep_Q_Network(state_size, action_size)
+
+done =False
+
+for e in range(n_episode):
+    state = env.reset()
+    state = np.reshape(state, [1,state_size])
+
+    for time in range(10000):
+        env.render()
+        # select an action based on epsilon given a the last state the selection action is played using take_action method
+        #which select random action from a given action set
+        action = agent.take_action(state)
+        #observe reward and next state
+        #done is a boolean flag to tell if it either the end of the game by lossing or the end of the episodo
+        next_state, reward, done, info = env.step(action)
+        #storing the reward equal to the returned reward from the environment if we are not done
+        reward = reward if not done else -10
+        #storing the received next state and saving it, the reshape is to transpose the next state to match our neural model
+        next_state = np.reshape(next_state, [1, state_size])
+        #store our experiences in experience_table (replay_memory)
+        agent.store_experience(state, action,reward, next_state, done)
+        #mapping the next state to be the current state
+        state = next_state
+        #If the game is finish before our episodes as the agent lost break
+        if done:
+            print ("episode:{}/{}, score:{:}".format(e, n_episode, time, agent.epsilon))
+            break
+        # sample a set of our experience replay according to the batch size
+        if len(agent.experience_table) > batch_size :
+                agent.replay(batch_size)
+
+        if e % 50 == 0:
+            agent.save(OP_dir + "weights" + "{:04d}.format(e)" + ".hdf5")
+    env.close()
